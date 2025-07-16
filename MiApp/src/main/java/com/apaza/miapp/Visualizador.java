4330f02e45c8fadb4cdba7c4182fcd20c9dbd6c4
apaza610 <homar.orozco@gmail.com>
2025-07-16 15:16:00 -0400

    button and TextField hidden, svg dimensions query to set window size

--- MiApp/src/main/java/com/apaza/miapp/Visualizador.java
+++ MiApp/src/main/java/com/apaza/miapp/Visualizador.java
@@ -4,22 +4,59 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 
 public class Visualizador extends Application {
     public static String elpath = "";
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private static double ancho;
+    private static double alto;
+    static Stage stagetmp;
+    static Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Visualizador.class.getResource("Visualizador.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 200, 200);
+        getAnchoAltoDeSVG(elpath);
+        scene = new Scene(fxmlLoader.load(), ancho, alto);
         primaryStage.setTitle("svg viewer");
         primaryStage.setScene(scene);
+        stagetmp = primaryStage;
         primaryStage.show();
+    }
+
+    public static void cambioAnchoAlto(int ancho, int alto){
+        stagetmp.setWidth(ancho);
+        stagetmp.setHeight(alto);
+    }
+
+    public static void main(String[] args) {
+        elpath = args[0];
+//        elpath = "D:\\apz\\pruebas\\juegos\\並.svg";
+//        elpath = "D:\\apz\\maps\\progr\\leng\\java\\0libs\\javafx\\0fotos\\basico_holamundo.svg";
+        launch();
+    }
+
+    public static void getAnchoAltoDeSVG(String elpath){
+        try {
+            File svgFile = new File(elpath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(svgFile);
+            Element svgElement = doc.getDocumentElement();
+
+            ancho = Double.parseDouble(svgElement.getAttribute("width").replace("px","")) +1;
+            alto = Double.parseDouble(svgElement.getAttribute("height").replace("px","")) +1;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- MiApp/src/main/java/com/apaza/miapp/VisualizadorCtrl.java
+++ MiApp/src/main/java/com/apaza/miapp/VisualizadorCtrl.java
@@ -3,40 +3,37 @@ package com.apaza.miapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static com.apaza.miapp.Visualizador.elpath;
+
 public class VisualizadorCtrl implements Initializable {
 
+    @FXML private Button btnGo;
     @FXML private TextField tfInput;
     @FXML private WebView wvNavegador;
 
     private WebEngine motor;
-    String elpath;
 
     @FXML
-    void goToSite() {
-        elpath = tfInput.getText();
-        motor.load(elpath);
-    }
-
-
-    @FXML
-    void onFit(ActionEvent event) {
-        System.out.println("pepepe");
-
+    void goToSite(ActionEvent event) {
+        String elpath = tfInput.getText();
+        File svgFile = new File(elpath);
+        String url = svgFile.toURI().toString();
+        motor.load(url);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         motor = wvNavegador.getEngine();
-        elpath = Visualizador.elpath;
         tfInput.setText(elpath);
-        goToSite();
+        btnGo.fire();
     }
-
 }
