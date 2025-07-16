78f85e42d397199ffbb276ff81beed081ba29929
apaza610 <homar.orozco@gmail.com>
2025-07-15 22:23:30 -0400

    webView grows till whole app is covered

--- MiApp/src/main/java/com/apaza/miapp/Visualizador.java
+++ MiApp/src/main/java/com/apaza/miapp/Visualizador.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Visualizador extends Application {
+    public static String elpath = "";
 
     public static void main(String[] args) {
         launch(args);
--- MiApp/src/main/java/com/apaza/miapp/VisualizadorCtrl.java
+++ MiApp/src/main/java/com/apaza/miapp/VisualizadorCtrl.java
@@ -2,21 +2,41 @@ package com.apaza.miapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
-public class VisualizadorCtrl {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VisualizadorCtrl implements Initializable {
+
+    @FXML private TextField tfInput;
+    @FXML private WebView wvNavegador;
+
+    private WebEngine motor;
+    String elpath;
 
     @FXML
-    private TextField tfInput;
-
-    @FXML
-    void goToSite(ActionEvent event) {
-
+    void goToSite() {
+        elpath = tfInput.getText();
+        motor.load(elpath);
     }
+
 
     @FXML
     void onFit(ActionEvent event) {
+        System.out.println("pepepe");
 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        motor = wvNavegador.getEngine();
+        elpath = Visualizador.elpath;
+        tfInput.setText(elpath);
+        goToSite();
     }
 
 }
